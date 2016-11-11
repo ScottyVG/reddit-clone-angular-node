f56176c51c1a07001f661438d13ebf0285874dd3
scottyvg <svangilder@gmail.com>
2016-11-10 22:56:59 -0700

    lets try it again

--- server/knexfile.js
+++ server/knexfile.js
@@ -8,20 +8,20 @@ module.exports = {
     client: 'pg',
     connection: 'postgres://localhost/reddit-clone-app',
     migrations: {
-      directory: './server/db/migrations',
+      directory: './db/migrations',
     },
     seeds: {
-      directory: './server/db/seeds',
+      directory: './db/seeds',
     },
   },
   production: {
     client: 'pg',
     connection: `${process.env.DATABASE_URL}?ssl=true`,
     migrations: {
-      directory: './server/db/migrations',
+      directory: './db/migrations',
     },
     seeds: {
-      directory: './server/db/seeds',
+      directory: './db/seeds',
     },
   },
 };
