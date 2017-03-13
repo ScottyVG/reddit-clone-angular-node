e4e1f0ea0f19e157533fa80e52b2b121819bb1ed
scottyvg <svangilder@gmail.com>
2017-03-13 08:16:13 -0600

    add NODE_ENV to knex file

--- server/db/knex.js
+++ server/db/knex.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const environment = process.env.ENV || 'development';
+const environment = process.env.NODE_ENV || 'development';
 const config = require('../knexfile.js')[environment];
 const knex = require('knex')(config);
 
